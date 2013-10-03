5adc1abf535983db448d3b12319b5c0f9ffc9b6a
Cyberpew <root@cyberpew.me>
2013-10-03 07:30:47 -0700

    MOAR DEBUG

--- src/me/xir/moovr/MoovrInit.java
+++ src/me/xir/moovr/MoovrInit.java
@@ -1,14 +1,24 @@
 package me.xir.moovr;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MoovrInit extends JavaPlugin {
+public class MoovrInit extends JavaPlugin implements Listener {
+	
+	public void onEnable(){
+		Bukkit.getLogger().info("Initialized.");
+	}
+	
+	public void onDisable(){
+		
+	}
 	
 	public Player sender;
 	@EventHandler

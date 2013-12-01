9ef37fdd40285431c5864815b2e0e1dd2f376532
Cyberpew <root@cyberpew.me>
2013-12-01 11:25:34 -0800

    do you even import bro

--- src/me/cyberpew/moovr/Moovr.java
+++ src/me/cyberpew/moovr/Moovr.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
 
 public class Moovr extends JavaPlugin implements Listener {
 	

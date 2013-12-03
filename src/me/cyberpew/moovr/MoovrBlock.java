ceb34b9891e3711f6e5d153109825d4de83d8204
Cyberpew <root@cyberpew.me>
2013-12-03 11:37:32 -0800

    Reformat

--- src/me/cyberpew/moovr/MoovrBlock.java
+++ src/me/cyberpew/moovr/MoovrBlock.java
@@ -9,36 +9,39 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class MoovrBlock implements Listener {
-	
-private Moovr plugin;
 
-public MoovrBlock(Moovr p) {
-	plugin = p;
-}
-	
+	private Moovr plugin;
+
+	public MoovrBlock(Moovr p) {
+		plugin = p;
+	}
+
 	@EventHandler
 	public void onPlayerMoveEvent(PlayerMoveEvent event) {
-		
-		event.getFrom().getBlock().getLocation().equals(event.getTo().getBlock().getLocation());
+
+		event.getFrom().getBlock().getLocation()
+				.equals(event.getTo().getBlock().getLocation());
 		Player player = event.getPlayer();
 		Block block = player.getLocation().getBlock();
 		Block blockAbove = block.getRelative(BlockFace.UP);
 		Block blockJump = block.getRelative(BlockFace.UP, 1);
-		if(player.hasPermission("moovr.use") || player.getPlayer().isOp()){
-				if(blockAbove.getType() == Material.AIR || blockJump.getType() == Material.AIR) {
-					if(block.getType() == Material.POWERED_RAIL) {
-						Block blockUnder = block.getRelative(BlockFace.DOWN);
-						if(blockUnder.getType() == Material.GOLD_BLOCK){
-							if (player.hasPermission("moovr.use")){
-								float walkspeed = (float) plugin.getConfig().getDouble("moovr.walkspeed");
-								player.setWalkSpeed(walkspeed);
-							}
+		if (player.hasPermission("moovr.use") || player.getPlayer().isOp()) {
+			if (blockAbove.getType() == Material.AIR
+					|| blockJump.getType() == Material.AIR) {
+				if (block.getType() == Material.POWERED_RAIL) {
+					Block blockUnder = block.getRelative(BlockFace.DOWN);
+					if (blockUnder.getType() == Material.GOLD_BLOCK) {
+						if (player.hasPermission("moovr.use")) {
+							float walkspeed = (float) plugin.getConfig()
+									.getDouble("moovr.walkspeed");
+							player.setWalkSpeed(walkspeed);
 						}
-					}else{
-						float defaultwalkspeed = 0.2F;
-						player.setWalkSpeed(defaultwalkspeed);
 					}
+				} else {
+					float defaultwalkspeed = 0.2F;
+					player.setWalkSpeed(defaultwalkspeed);
 				}
+			}
 		}
 	}
 }
